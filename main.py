7943def63231eb0433e5dc5284983abb53fc41b6
exodiast. <caraccurse@gmail.com>
2025-09-05 18:31:59 +0000

    modified:   .gitignore 	modified:   database.py 	modified:   main.py 	modified:   requirements.txt 	modified:   routers/admin_commands.py 	modified:   routers/user_commands.py

--- main.py
+++ main.py
@@ -6,10 +6,11 @@ from database import create_table, add_new_columns, initialize_admins
 from aiogram.client.default import DefaultBotProperties
 from aiogram.enums import ParseMode
 from routers.admin_commands import ADMIN_IDS
+from dotenv import load_dotenv
+import os
 
-
-TOKEN = '6695001037:AAG2GsxtOZcFFlvQ9jCzLQy3IhNrSkxmV2Y' # Основной бот
-# TOKEN = '7753431963:AAGWJK3j1XvDrYgFjFxneWvHLV5iBBUOBeA' # Тестовый бот | Для Кая
+load_dotenv()
+token=os.getenv('TOKEN')
 
 bot=Bot(TOKEN, default=DefaultBotProperties(parse_mode=ParseMode.HTML, link_preview_is_disabled=True)) # Подключение бота(с оформлением смс)
 dp=Dispatcher()
